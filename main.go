adac931d0fe0bab34b18cddd6cc5243c649d3054
blavibutcher <rileydouglas@outlook.com>
2020-12-28 21:15:12 +1300

    Finished up rotation error. There's now a default size. Still need to fiddle with numbers to get it fitting to screen nicely.

--- main.go
+++ main.go
@@ -18,27 +18,38 @@ func main() {
 	characters := strings.Split(asciiString, "")
 
 	mw := imagick.NewMagickWand()
-	err := mw.ReadImage("smiley.png")
+	err := mw.ReadImage("teArohaClock.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Sucessfully loaded image!")
+
+	// func (mw *MagickWand) AdaptiveResizeImage(cols, rows uint) error
+	err = mw.AdaptiveResizeImage(500, 200)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	height := mw.GetImageHeight()
 	width := mw.GetImageWidth()
 	fmt.Printf("Image size: %d x %d\n", width, height)
 
+	// can't set final in golang, so using very large numbers as array bounds
 	var imagePixels [10000][10000]int
+
 	//func (mw *MagickWand) GetImagePixelColor(x, y int) (color *PixelWand, err error)
 	for i := 0; i < int(height); i++ {
 		for j := 0; j < int(width); j++ {
-			pw, err := mw.GetImagePixelColor(i, j)
+			pw, err := mw.GetImagePixelColor(j, i)
 			if err != nil {
 				log.Fatal(err)
 			}
-			imagePixels[i][j] = averageBrightness(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
-			asciiIndex := math.Floor(float64(imagePixels[i][j]) / 3.92)
-			fmt.Print(characters[int(asciiIndex)])
-			fmt.Print(characters[int(asciiIndex)])
+			// imagePixels[i][j] = averageBrightness(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
+			// imagePixels[i][j] = lightness(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
+			imagePixels[i][j] = luminosity(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
+			asciiIndex := math.Floor(float64(imagePixels[i][j]) / 3.93)
+			// fmt.Print(characters[int(asciiIndex)])
+			// fmt.Print(characters[int(asciiIndex)])
 			fmt.Print(characters[int(asciiIndex)])
 			
 		}
@@ -55,6 +66,16 @@ func mapToRgb(color float64) float64 {
 func averageBrightness(r, g, b float64) int {
 	
 	return (int(r) + int(g) + int(b)) / 3
+}
+
+func lightness(r, g, b float64) int {
+	value := math.Max(math.Max(r, g), b)
+	return int(value)
+}
+
+func luminosity(r, g, b float64) int {
+	value := 0.21 * r + 0.72 * g + 0.07 * b
+	return int(value)
 }
 
 
