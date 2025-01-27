e4dbb20f60bc3f6fe3f53a423818cf53aeb65422
Riley Douglas <rileydouglas@outlook.com>
2025-01-27 17:08:28 +1300

    auto ratio and enter img path

---
+++ go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.20
+
+require gopkg.in/gographics/imagick.v2 v2.7.0
+
+require (
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
 	"math"
+	"os"
+	"strings"
 
+	"golang.org/x/term"
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
@@ -13,45 +15,57 @@ func main() {
 	imagick.Initialize()
 	defer imagick.Terminate()
 
-	// Split asscii string. These are the characters from lightest to darkest
+	// Check if an image path is provided as a command-line argument
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an image path as an argument.")
+		fmt.Println("Usage: go run main.go <image_path>")
+		return
+	}
+
+	imagePath := os.Args[1]
+
+	// Split ascii string. These are the characters from lightest to darkest
 	asciiString := "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 	characters := strings.Split(asciiString, "")
 
 	mw := imagick.NewMagickWand()
-	err := mw.ReadImage("teArohaClock.jpg")
+	err := mw.ReadImage(imagePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Sucessfully loaded image!")
+	fmt.Println("Successfully loaded image!")
 
-	// func (mw *MagickWand) AdaptiveResizeImage(cols, rows uint) error
-	err = mw.AdaptiveResizeImage(500, 200)
+	// Get terminal width
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		width = 80 // Default to 80 if unable to get terminal width
+	}
 
+	// Calculate adaptive height
+	originalWidth := mw.GetImageWidth()
+	originalHeight := mw.GetImageHeight()
+	aspectRatio := float64(originalHeight) / float64(originalWidth)
+	newWidth := uint(width)
+	newHeight := uint(float64(newWidth) * aspectRatio * 0.5) // Multiply by 0.5 to account for terminal line height
+
+	err = mw.AdaptiveResizeImage(newWidth, newHeight)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	height := mw.GetImageHeight()
-	width := mw.GetImageWidth()
-	fmt.Printf("Image size: %d x %d\n", width, height)
+	width = int(mw.GetImageWidth())
+	fmt.Printf("ASCII Art size: %d x %d\n", width, height)
 
-	// can't set final in golang, so using very large numbers as array bounds
-	var imagePixels [10000][10000]int
-
-	//func (mw *MagickWand) GetImagePixelColor(x, y int) (color *PixelWand, err error)
 	for i := 0; i < int(height); i++ {
 		for j := 0; j < int(width); j++ {
 			pw, err := mw.GetImagePixelColor(j, i)
 			if err != nil {
 				log.Fatal(err)
 			}
-			// imagePixels[i][j] = averageBrightness(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
-			// imagePixels[i][j] = lightness(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
-			imagePixels[i][j] = luminosity(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
-			asciiIndex := math.Floor(float64(imagePixels[i][j]) / 3.93)
-			// fmt.Print(characters[int(asciiIndex)])
-			// fmt.Print(characters[int(asciiIndex)])
+			luminosityValue := luminosity(mapToRgb(pw.GetRed()), mapToRgb(pw.GetGreen()), mapToRgb(pw.GetBlue()))
+			asciiIndex := math.Floor(float64(luminosityValue) / 3.93)
 			fmt.Print(characters[int(asciiIndex)])
-			
 		}
 		fmt.Println("")
 	}
@@ -62,22 +76,7 @@ func mapToRgb(color float64) float64 {
 	return color * 255
 }
 
-// Gets a brightness value for the current pixel
-func averageBrightness(r, g, b float64) int {
-	
-	return (int(r) + int(g) + int(b)) / 3
-}
-
-func lightness(r, g, b float64) int {
-	value := math.Max(math.Max(r, g), b)
-	return int(value)
-}
-
 func luminosity(r, g, b float64) int {
-	value := 0.21 * r + 0.72 * g + 0.07 * b
+	value := 0.21*r + 0.72*g + 0.07*b
 	return int(value)
 }
-
-
-// Lightness: average the maximum and minimum values out of R, G and B - max(R, G, B) + min(R, G, B) / 2
-// Luminosity: take a weighted average of the R, G and B values to account for human perception - 0.21 R + 0.72 G + 0.07 B
